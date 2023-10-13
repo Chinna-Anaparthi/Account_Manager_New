267950d77be7345568497b679115aefbf57e8238
Chinna-Anaparthi <123636616+Chinna-Anaparthi@users.noreply.github.com>
2023-10-13 20:24:57 +0530

    third commit

--- Server_Api.js
+++ Server_Api.js
@@ -13,7 +13,7 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    console.log("hosting MongoDB Connected");
+    console.log("Account_Manager MongoDB Connected");
   })
   .catch((error) => {
     console.log(error);
